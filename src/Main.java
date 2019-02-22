9056cdb2296f969e6c31478ae50813d63deb3f06
Linnea <bark_linnea@hotmail.com>
2019-02-22 08:39:18 +0100

    Cleaned

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
-        int var = 0;
-
-        boolean isIt = true;
-
-        String hej = "hej";
+        
     }
 }
